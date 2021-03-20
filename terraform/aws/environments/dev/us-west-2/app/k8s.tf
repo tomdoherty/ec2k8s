00c61e3ad515b1f6b5c314c45f22f3167c4f924e
Tom Doherty <tom@singlesecond.com>
2021-03-20 23:37:42 +0000

    feat(terraform): read local ssh public key

--- terraform/aws/environments/dev/us-west-2/app/k8s.tf
+++ terraform/aws/environments/dev/us-west-2/app/k8s.tf
@@ -32,7 +32,7 @@ module "k8s-dev" {
   controller_ami  = data.aws_ami.ami-k8s.id
   controller_size = "t2.micro"
   ingress_dns     = "k8s.tom.works"
-  ssh_public_key  = "ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAABAQC9FNpoDiJLd+if9noTjimmiCfTi0BUa3uQFnUOf5PVLx+gT0+61j7+EOvvqdVN8pUI/+eNMJPqDvrPsKqe63QJkDboltJaY9m39KAPAVw/L8myLDsxcXprmLOtK8MlHc1FvGwsUeiZAZaEdt/KfOd/zkU/qd5xpQVk9ERO/H+o3T5ReuEV63vlSnF8mXvh5gFzJVLiTgMgGhYizg24Z894nalGx+rvPz1XWVhEqlZsQsdyXQsnUdoboSyVw1tcN3y87Tws8k72ZRMd5Yc9zs+5XN3Yj4DOtJzac0wvcFAVIetHMz2BWUbT5Ei9BDAjGerI+nr47p5CDetyqy82Ctwz tom@Thomass-MacBook-Pro.local"
+  ssh_public_key  = file("~/.ssh/id_rsa.pub")
   worker_ami      = data.aws_ami.ami-k8s.id
   worker_count    = 3
   worker_size     = "t2.micro"
